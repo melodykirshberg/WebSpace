1559fbef15b45a6d5305f95d0ccbbc0d655251d3
Cibelle Montor <you@example.com>
2019-12-31 14:42:47 -0700

    added homepage,login page and create routes to them

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import Home from "./Pages/Home"
+import Login from "./Pages/Login"
+import Register from "./Pages/Register"
+import Footer from "./components/Footer"
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
 function App() {
+
+
   return (
-    <div className="App">
-      <h2>Our web goes here!!</h2>
-    </div>
+    <Router>
+      <div className="App">
+
+
+        {/* <Home /> */}
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
