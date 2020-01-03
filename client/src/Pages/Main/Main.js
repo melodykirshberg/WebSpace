f51ad8f16da1a063782774dd486f0bc7b19ee47c
TJ Cheetham <cheetham.tj@gmail.com>
2020-01-03 14:13:55 -0700

    house cleaning

--- client/src/Pages/Main/Main.js
+++ client/src/Pages/Main/Main.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import NavBarMain from "../../components/NavBarMain";
-import MainComponent from "../../components/MainComponent";
-import Wrapper from "../../components/Wrapper";
-import Header from "../../components/Header";
 import UserModal from "../../components/UserModal"
 import API from "../../utils/API";
 
@@ -10,31 +7,27 @@ import API from "../../utils/API";
 // when page loads, I want to load all users in the database and pass as props until it gets to the modal
 class Main extends Component {
 
-    // Setting the component's initial state
+  // Setting the component's initial state
 
-    state = {
-        users: []
-    }
-
-
-    // this is the initialization, what do you want the page to display when page it's first loaded When page it's loaded go make an API call to our DB and get all USERS
-    componentDidMount() {
-        API.getUsers().then(res => this.setState({
-            users: res.data.results
-        })).catch(err => console.log(err))
-    }
+  state = {
+    users: []
+  }
 
 
+  // this is the initialization, what do you want the page to display when page it's first loaded When page it's loaded go make an API call to our DB and get all USERS
+  componentDidMount() {
+    API.getUsers().then(res => this.setState({
+      users: res.data.results
+    })).catch(err => console.log(err))
+  }
 
-    render() {
-        return (
-            <span>
-                <NavBarMain />
-                {/* <Wrapper>  */}
-                <UserModal />
-                {/* <MainComponent /> */} */}
-                {/* </Wrapper> */}
 
+
+  render() {
+    return (
+      <span>
+        <NavBarMain />
+        <UserModal />
       </span>
 
     )
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,3 @@
-// import axios from "axios";
-// export default {
-//     // Gets all users
-//     getUsers: function () {
-//         return axios.get("https://randomuser.me/api/?results=200&nat=us");
-//     }
-// };
 import axios from "axios";
 
 export default {
