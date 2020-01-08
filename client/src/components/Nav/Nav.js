4e3cfd86ff915e66cb170ae2333632a24ed2015b
verlitas <55808608+verlitas@users.noreply.github.com>
2020-01-08 10:46:55 -0700

    nav working

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -19,11 +19,11 @@ function Nav(props) {
 
     return (
         <nav className="nav" onClick={() => props.updateFormState("base")}>
-            <h1 style={styles.navTitle}>Web Space</h1>
+            <h1 className="navTitle">Web Space</h1>
             {state.user && state.user.signInUserSession && (
                 <div>
-                    <h4 style={styles.navGreet}>Welcome {state.user.signInUserSession.idToken.payload.name}</h4>
-                    <button style={styles.navSignOut} onClick={signOut}>
+                    <h4 className="navGreet">Welcome {state.user.signInUserSession.idToken.payload.name}</h4>
+                    <button className="navSignOut" onClick={signOut}>
                         <FaSignOutAlt color="grey" />
                         <p className="text">Sign Out</p>
                     </button>
