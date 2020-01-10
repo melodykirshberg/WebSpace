50337975f4cc701cf584596e96995a780b8cac11
verlitas <55808608+verlitas@users.noreply.github.com>
2020-01-10 12:09:04 -0700

    removed build script

---
+++ client/build/precache-manifest.c9d1de04f26f62d31226f859dd691d4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ef0e478f6ef7b5f31afd0ab4f8e7ec3",
+    "url": "/static/media/image1.4ef0e478.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a88588565251175c76af",
+    "url": "/static/js/main.277a3525.chunk.js"
+  },
+  {
+    "revision": "f19d25854dd1589d18ca",
+    "url": "/static/js/2.3175ca8d.chunk.js"
+  },
+  {
+    "revision": "a88588565251175c76af",
+    "url": "/static/css/main.21631f8c.chunk.css"
+  },
+  {
+    "revision": "ebf9e556b3a508cf8cdac262a7cae70f",
+    "url": "/index.html"
+  }
+];
