5e615514b5397d2885eefb6c8a6fd2d7a114f35c
verlitas <55808608+verlitas@users.noreply.github.com>
2020-01-12 18:15:13 -0700

    fixed wording

--- client/src/components/Button/Buttons.js
+++ client/src/components/Button/Buttons.js
@@ -9,7 +9,7 @@ function Buttons(props) {
       {/* <h2 className="bTitle">Web Space</h2> */}
       <img src={require("./image1.png")} className="bPic" />
       <h4 className="bBio">
-        Get to know better the people you are about to network with
+      Build and engage in your personal network circle
       </h4>
       <div className="text-center">
         <button
@@ -17,7 +17,7 @@ function Buttons(props) {
           onClick={() => Auth.federatedSignIn({ provider: "Google" })}
         >
           <FaGoogle className="mx-2 my-2" color="red" />
-          <p className="bGoogletext  my-2">Sign Up with Google</p>
+          <p className="bGoogletext  my-2">Sign In with Google</p>
         </button>
       </div>
 
