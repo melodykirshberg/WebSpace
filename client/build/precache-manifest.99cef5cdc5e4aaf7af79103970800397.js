7aa665660bc48f9acca28f85c90c9b8381f99e19
verlitas <55808608+verlitas@users.noreply.github.com>
2020-02-18 04:26:48 -0700

    updating build

---
+++ client/build/precache-manifest.99cef5cdc5e4aaf7af79103970800397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91fc15e58a1c8ac2e530bc1022de9f7a",
+    "url": "/static/media/WebSpace.91fc15e5.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "57a05aad463a1fbc560e",
+    "url": "/static/js/main.804d3f46.chunk.js"
+  },
+  {
+    "revision": "1bd9eccc2a496fb2f383",
+    "url": "/static/js/2.7c28dce5.chunk.js"
+  },
+  {
+    "revision": "57a05aad463a1fbc560e",
+    "url": "/static/css/main.3235b6ce.chunk.css"
+  },
+  {
+    "revision": "ed5d3313b6a113d2998fa188cc6f16a2",
+    "url": "/index.html"
+  }
+];
